d1e7ece097429b99f609721b104a03ce42852784
Kenneth Plackowski <kennethplackowski@kenneths-air.unm.edu>
2022-03-15 10:12:21 -0600

    edited loadcheck.py. I wouldnt run it with a big set of beta if you are checking for functionality

--- Project_2 copy/loadcheck.py
+++ Project_2 copy/loadcheck.py
@@ -5,6 +5,9 @@ from scipy import sparse
 from scipy.sparse import linalg
 from sklearn.model_selection import train_test_split
 import sys
+import time
+import matplotlib.pyplot as plt
+start_time = time.time()
 np.set_printoptions(threshold=sys.maxsize)
 
 #read in vocabular and labels text files. Can convert to sparse if needed.
@@ -23,51 +26,19 @@ training_data = sparse.load_npz('final_training_sparse.npz')
 #20% split of the training data into training and validation.
 training_X, test_X, training_Y, test_Y = train_test_split(training_data[:,1:-1], training_data[:,-1], test_size = 0.2, random_state = 40)
 
+#########################################
+#THIS is only for testing time expected for program to finish by step size of beta.
+#beta_length = []
+#for B in range(1,100002,2000):
+#    print(B)
+#    beta_length.append(B/100000)
+#print(beta_length[0], beta_length[-1])
+#print(len(beta_length))
+#quit()
+#########################################
 
-######construction of the MAP P(X|Y).
-
-#beta will be 1/abs_V. We can change this and incorporate into a loop for more betas.
-abs_V = vocabulary.shape[0]
-beta = 1/abs_V
-
-
-#this retains the position of each class in the training set, to use to sum up all xi in each class.
-total_length = 0
-#converting a list of betas into one sparse matrix for sparse matrix operations (addition is not supported elementwise as far as i can tell.
-add_factor = [beta]*len(vocabulary)
-add_factor = sparse.csr_matrix(add_factor)
-
-for y in labels.index:
-    Ypositions = []
-
-    for i in range(1, training_Y.indptr.shape[0]):
-        if (training_Y==(y+1)).indptr[i-1] == (training_Y==(y+1)).indptr[i]-1:
-            Ypositions.append(i-1)
-    total_length += len(Ypositions)
-    #sums ALL xis, and gives a 1xlen(vocabulary) list of sums for class y+1
-    sum_xi = training_X[Ypositions, :].sum(axis=0)
-    #sums ALL words (aka sum of sums of xis) in the yk class
-    sum_yk = training_X[Ypositions, :].sum()
-    sum_xi = sparse.csr_matrix(sum_xi)
-    
-    #this is the P(Xi, Yk) row for each x_i with a dirichlet MAP estimation.
-    xi_dirichlet = (sum_xi+add_factor)/(sum_yk+beta*abs_V)
-    #added this in just to check if it adds to 1. It adds to very near 1 for all cases.
-    #print(((sum_xi+add_factor)/(sum_yk+beta*abs_V)).sum())
-    
-    #basecase to initialize our sparse matrix
-    if y == 0:
-        MAP_estimate = xi_dirichlet
-    else:
-        MAP_estimate = sparse.vstack((MAP_estimate, xi_dirichlet))
-
-#this checks that the MAP matrix is indeed 20 x 61188, classes(yk) by words(xi)
-#print(MAP_estimate.shape)
-#this changes MAP_estimate into its log for training.
-MAP_estimate.data = np.log(MAP_estimate.data)
-print(MAP_estimate.shape)
-        
-
+####construction of MAE for P(Y)
+#construct this first, since this is independent of beta. Saves a ton of time when looping over different beta.
 vector_totals = []
 for y in labels.index:
     totals = 0
@@ -80,19 +51,86 @@ PY_MAE = vector_totals/sparse.csr_matrix.sum(vector_totals)
 PY_MAE.data = np.log(PY_MAE.data)
 print(PY_MAE.shape)
 
+######construction of the MAP P(X|Y).
 
-#argmax should be a vector based function of the form log(P(Y)_MAE)+X_i*P(X|Y)_MAP^T, and result in a vector of size 1x20.
-#Test_X is a 2400 x 61188 matrix. so take each ROW and use it to make a prediction. Those rows are 1x61188.
-predictions = []
-for x in range(0,test_X.shape[0]):
-    arg_search = PY_MAE + test_X[x, :]*MAP_estimate.transpose()
-    predictions.append(arg_search.argmax(axis = 1)+1)
+#beta will be 1/abs_V. We can change this and incorporate into a loop for more betas. (this instantiates the beta to be the size of 1/abs_V. Ill comment this out and make a list of betas to loop the training function over. The accuracy is on the validation set from the split above.
+abs_V = vocabulary.shape[0]
+#beta = 1/abs_V
+accuracy_probs = []
+beta_vector = []
 
-count_correct = 0
-for p in range(0,len(predictions)):
-    if predictions[p] == test_Y[p, 0]:
-        count_correct += 1
+
+#this retains the position of each class in the training set, to use to sum up all xi in each class.
+total_length = 0
+
+#You get 51 data points
+step_count = 0
+for B in range(0, 1001, 500):
+    beta = B/100000
+    beta_vector.append(beta)
+    for y in labels.index:
+        Ypositions = []
+
+        for i in range(1, training_Y.indptr.shape[0]):
+            if (training_Y==(y+1)).indptr[i-1] == (training_Y==(y+1)).indptr[i]-1:
+                Ypositions.append(i-1)
+        total_length += len(Ypositions)
+        #sums ALL xis, and gives a 1xlen(vocabulary) list of sums for class y+1
+        sum_xi = training_X[Ypositions, :].sum(axis=0)
+        #sums ALL words (aka sum of sums of xis) in the yk class
+        sum_yk = training_X[Ypositions, :].sum()
+        sum_xi = sparse.csr_matrix(sum_xi)
         
-print(count_correct)
+        #this is the P(Xi, Yk) row for each x_i with a dirichlet MAP estimation.
+        #converting a list of betas into one sparse matrix for sparse matrix operations (addition is not supported elementwise as far as i can tell.
+        add_factor = [beta]*len(vocabulary)
+        add_factor = sparse.csr_matrix(add_factor)
+        xi_dirichlet = (sum_xi+add_factor)/(sum_yk+beta*abs_V)
+        #added this in just to check if it adds to 1. It adds to very near 1 for all cases.
+        #print(((sum_xi+add_factor)/(sum_yk+beta*abs_V)).sum())
+        
+        #basecase to initialize our sparse matrix
+        if y == 0:
+            MAP_estimate = xi_dirichlet
+        else:
+            MAP_estimate = sparse.vstack((MAP_estimate, xi_dirichlet))
 
+    #this checks that the MAP matrix is indeed 20 x 61188, classes(yk) by words(xi)
+    #print(MAP_estimate.shape)
+    #this changes MAP_estimate into its log for training.
+    MAP_estimate.data = np.log(MAP_estimate.data)
+    print(MAP_estimate.shape)
+            
+
+
+
+
+    #argmax should be a vector based function of the form log(P(Y)_MAE)+X_i*P(X|Y)_MAP^T, and result in a vector of size 1x20.
+    #Test_X is a 2400 x 61188 matrix. so take each ROW and use it to make a prediction. Those rows are 1x61188.
+    predictions = []
+    for x in range(0,test_X.shape[0]):
+        arg_search = PY_MAE + test_X[x, :]*MAP_estimate.transpose()
+        predictions.append(arg_search.argmax(axis = 1)+1)
+
+    count_correct = 0
+    for p in range(0,len(predictions)):
+        if predictions[p] == test_Y[p, 0]:
+            count_correct += 1
+            
+    accuracy_probs.append(count_correct/test_X.shape[0])
+    step_count += 1
+    print(step_count)
+
+end_time = time.time()
+max_acc = max(accuracy_probs)
+print(max_acc)
+#max_beta = beta_vector.index(max_acc)
+plt.xscale("log")
+plt.plot(beta_vector, accuracy_probs)
+plt.xlabel("Beta")
+plt.ylabel("Accuracy")
+plt.title("Beta vs. Accuracy for Naive Bayes")
+plt.savefig("Beta_vs_Accuracy_for_Naive_Bayes_smaller.png")
+plt.show()
+print("this takes", end_time-start_time, "seconds to run")
     
